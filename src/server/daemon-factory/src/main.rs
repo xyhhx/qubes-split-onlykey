40c225becdd74c79f3f120c55611c80df03e194f
xyhhx <xyhhx@tuta.io>
2025-02-27 09:05:06 -0500

    feat(server/daemon-factory): set up basic daemon socket/service spawning

--- src/server/daemon-factory/src/main.rs
+++ src/server/daemon-factory/src/main.rs
@@ -1,17 +1,41 @@
+use std::env;
+
 use anyhow::Result;
-use daemon_factory::{log::install_systemd_journal_logger, zbus_proxy::watch_systemd_jobs};
-use log::LevelFilter;
+use tokio::io::{stdin, AsyncReadExt};
+
+use zbus::{proxy, zvariant::OwnedObjectPath, Connection};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  install_systemd_journal_logger(Some(LevelFilter::Trace))?;
-  watch_systemd_jobs().await?;
+  let connection = Connection::session().await?;
+  dbg!(&connection);
+  dbg!(env::vars());
 
-  // TODO: get job, wait for it to end
-  // TODO: remove it
-  // TODO: start new job using dbus
-  // ???
-  // profit
+  let mut input = String::new();
+  stdin().read_to_string(&mut input).await?;
+  dbg!(&input);
+  let unit = format!(
+    "daemon@{}.socket",
+    &input.trim_matches(|c| !char::is_alphanumeric(c) && !char::is_ascii_punctuation(&c))
+  );
+
+  let systemd_proxy = Systemd1ManagerProxy::new(&connection).await?;
+  let name = &unit;
+  let mode = "replace";
+  println!("calling start_unit with args:");
+  dbg!((name, mode));
+
+  let path = systemd_proxy.start_unit(name, mode).await?;
+  println!("started the daemon at job path {:?}", path);
 
   Ok(())
+}
+
+#[proxy(
+  interface = "org.freedesktop.systemd1.Manager",
+  default_service = "org.freedesktop.systemd1",
+  default_path = "/org/freedesktop/systemd1"
+)]
+trait Systemd1Manager {
+  fn start_unit(&self, name: &str, mode: &str) -> zbus::Result<OwnedObjectPath>;
 }
--- src/server/socket-proxy/src/main.rs
+++ src/server/socket-proxy/src/main.rs
@@ -7,6 +7,7 @@ use tokio::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
+  // Set up listener against file descriptor(s)
   let mut listenfd = listenfd::ListenFd::from_env();
   let listener = UnixListener::from_std(
     listenfd
@@ -14,6 +15,7 @@ async fn main() -> Result<()> {
       .expect("Couldn't create listener"),
   )?;
 
+  // Handle incoming connections
   while let Ok((mut sock, _)) = listener.accept().await {
     tokio::spawn(async move {
       handle_connection(&mut sock).await?;
@@ -25,11 +27,15 @@ async fn main() -> Result<()> {
 }
 
 async fn handle_connection(socket: &mut UnixStream) -> Result<()> {
+  // Set up buffers
   let mut initial_buf: Vec<u8> = vec![];
   let mut writer = BufWriter::new(stdout());
   let mut reader = BufReader::new(socket);
+
+  // Read the until header delimiter
   let n = reader.read_until(b'\0', &mut initial_buf).await?;
 
+  // Parse the header fields
   let [rpc_service, remote_domain] = str::from_utf8(&initial_buf[..n - 1])?
     .split(" ")
     .collect::<Vec<_>>()[..2]
@@ -37,11 +43,16 @@ async fn handle_connection(socket: &mut UnixStream) -> Result<()> {
     bail!("Couldn't read service descriptor header");
   };
 
+  // Logging
   println!(
     "qrexec call: rpc_service={:?} remote_domain={:?}",
     &rpc_service, &remote_domain
   );
 
+  // TODO: Spawn a new daemon, get its socket
+  // TODO: Proxy to that socket instead
+
+  // Connect reader/writer socket buffers
   copy(&mut reader, &mut writer).await?;
 
   Ok(())
