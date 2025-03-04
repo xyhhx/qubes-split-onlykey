bc09e84455513a4e2579ca92ac56aba9072bfcff
xyhhx <xyhhx@tuta.io>
2025-03-03 22:47:11 -0500

    chore: wip

--- src/server/daemon-factory/src/main.rs
+++ src/server/daemon-factory/src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 extern crate anyhow;
+extern crate sd_notify;
 extern crate tokio;
 extern crate zbus;
 use anyhow::Result;
+use sd_notify::NotifyState;
 use server_lib::Systemd1ManagerProxy;
 use tokio::io::{AsyncReadExt, stdin};
 use zbus::Connection;
@@ -12,23 +14,31 @@ async fn main() -> Result<()> {
   let connection = Connection::session().await?;
   dbg!(&connection);
   dbg!(env::vars());
+  // notify systemd and we're ready to start
+  sd_notify::notify(false, &[NotifyState::Ready])?;
 
+  // read stdin to get desired socket/service name
   let mut input = String::new();
   stdin().read_to_string(&mut input).await?;
   dbg!(&input);
+  // sanitization
   let unit = format!(
+    // TODO: This should be configurable
     "daemon@{}.socket",
     &input.trim_matches(|c| !char::is_alphanumeric(c) && !char::is_ascii_punctuation(&c))
   );
 
+  // start a new daemon instance with the requested name
   let systemd_proxy = Systemd1ManagerProxy::new(&connection).await?;
   let name = &unit;
   let mode = "replace";
   println!("calling start_unit with args:");
   dbg!((name, mode));
-
   let path = systemd_proxy.start_unit(name, mode).await?;
   println!("started the daemon at job path {:?}", path);
+
+  // we're done
+  sd_notify::notify(false, &[NotifyState::Stopping])?;
 
   Ok(())
 }
--- src/server/socket-proxy/src/main.rs
+++ src/server/socket-proxy/src/main.rs
@@ -1,16 +1,19 @@
 extern crate anyhow;
+extern crate sd_notify;
 extern crate tokio;
 
 use core::str;
 use std::env;
+use std::io::ErrorKind;
 use std::os::unix::net::{SocketAddr, UnixStream as StdStream};
 use std::path::Path;
 
 use anyhow::{Error, Result, bail};
+use sd_notify::NotifyState;
 use server_lib::Systemd1ManagerProxy;
-use tokio::io::{AsyncBufReadExt, BufReader, BufWriter, copy, stdout};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter, Interest, copy, stdout};
 use tokio::net::{UnixListener, UnixStream};
-use zbus::Connection;
+use zbus::{Connection, MatchRule};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -21,6 +24,7 @@ async fn main() -> Result<()> {
       .take_unix_listener(0)?
       .expect("Couldn't create listener"),
   )?;
+  sd_notify::notify(false, &[NotifyState::Ready])?;
 
   // Handle incoming connections
   while let Ok((mut sock, _)) = listener.accept().await {
@@ -34,47 +38,14 @@ async fn main() -> Result<()> {
 }
 
 async fn handle_connection(incoming_rpc_socket: &mut UnixStream) -> Result<()> {
-  // Set up buffers
-  let mut initial_buf: Vec<u8> = vec![];
   let mut writer = BufWriter::new(stdout());
-  let mut reader = BufReader::new(incoming_rpc_socket);
-
-  // Read the until header delimiter
-  let n = reader.read_until(b'\0', &mut initial_buf).await?;
-
-  // Parse the header fields
-  let [rpc_service, remote_domain] = str::from_utf8(&initial_buf[..n - 1])?
-    .split(" ")
-    .collect::<Vec<_>>()[..2]
-  else {
-    bail!("Couldn't read service descriptor header");
-  };
-
-  // Logging
-  println!(
-    "qrexec call: rpc_service={:?} remote_domain={:?}",
-    &rpc_service, &remote_domain
-  );
-
+  let rpc_call = get_rpc_info_from_socket_input(incoming_rpc_socket).await?;
   // TODO: validate input
 
   // TODO: Spawn a new daemon, get its socket
-
-  let factory_socket_addr = get_factory_socket_addr().await?;
-
-  let std_stream = StdStream::connect_addr(&factory_socket_addr)?;
-  let factory_socket = UnixStream::from_std(std_stream)?;
-
-  factory_socket.writable().await?;
-
-  factory_socket
-    .try_write(remote_domain.as_bytes())
-    .unwrap_err();
-
   let connection = Connection::session().await?;
   let systemd_proxy = Systemd1ManagerProxy::new(&connection).await?;
-
-  // systemd_proxy.start_unit(, mode)
+  handle_factory(rpc_call.remote_domain.as_str());
 
   // start a daemon factory: pass the remote domain name to the factory.socket
   // wait for it to finish, then find the spawned daemon socket/service
@@ -88,11 +59,60 @@ async fn handle_connection(incoming_rpc_socket: &mut UnixStream) -> Result<()> {
   Ok(())
 }
 
+async fn get_rpc_info_from_socket_input(
+  incoming_rpc_socket: &mut UnixStream,
+) -> Result<QubesRPCCall> {
+  // Set up buffers
+  let mut buffer: Vec<u8> = vec![];
+  let mut reader = BufReader::new(incoming_rpc_socket);
+
+  // Read the until header delimiter
+  let n = reader.read_until(b'\0', &mut buffer).await?;
+
+  // Parse the header fields
+  let [rpc_service, remote_domain] = str::from_utf8(&buffer[..n - 1])?
+    .split(" ")
+    .collect::<Vec<_>>()[..2]
+  else {
+    bail!("Couldn't read socket input into buffer")
+  };
+
+  Ok(QubesRPCCall {
+    remote_domain: String::from(remote_domain),
+    rpc_service: String::from(rpc_service),
+  })
+}
+
 async fn get_factory_socket_addr() -> Result<SocketAddr> {
   let runtime_path = env::var("RUNTIME_DIRECTORY")?;
   let path = Path::new(&runtime_path).with_file_name("daemon-factory.sock");
-
   let sock_addr = SocketAddr::from_pathname(path)?;
 
   Ok(sock_addr)
+}
+
+async fn make_match_rule() -> Result<MatchRule<'static>> {
+  Ok(
+    MatchRule::builder()
+      .msg_type(zbus::message::Type::Signal)
+      .sender("org.freedesktop.systemd1")?
+      .interface("org.freedesktop.systemd1.Manager")?
+      .member("JobRemoved")?
+      .build(),
+  )
+}
+
+async fn handle_factory(remote_domain: &str) -> Result<&str> {
+  let socket_addr = get_factory_socket_addr().await?;
+  let std_stream = StdStream::connect_addr(&socket_addr)?;
+  let stream = UnixStream::from_std(std_stream)?;
+
+  // TODO: write the remote_domain to the socket
+  // TODO: wait for output from socket, read value (the job path of the new daemon service)
+  // TODO: return the job path
+}
+
+struct QubesRPCCall {
+  remote_domain: String,
+  rpc_service: String,
 }
